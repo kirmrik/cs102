430bfe9605dcacc07c3e0ab77ead5f89476db033
Kirill <kirmrik@gmail.com>
2018-12-14 22:52:48 +0300

    API VK had been added

---
+++ homework04/config.py
@@ -0,0 +1,11 @@
+VK_CONFIG = {
+    'domain': 'https://api.vk.com/method',
+    'access_token': '5d62306d5796a873583451f3eb8a717b019b18be26b7148ce957fcd35155433ada0f1ca819646dcdcb74f',
+    'version': '5.87',
+}
+
+
+PLOTLY_CONFIG = {
+    'username': 'KiRmRiK',
+    'api_key': 'tIuI9nTH0nv7mxkRLjUo',
+}
