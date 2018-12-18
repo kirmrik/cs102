4b05e6afc461c1d32703d2655af702ef422d83e5
Kirill <kirmrik@gmail.com>
2018-12-18 15:10:09 +0300

    API has been changed

--- homework04/messages.py
+++ homework04/messages.py
@@ -6,13 +6,11 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 from api import messages_get_history
-from api_models import Message
 import config
 
 
 Dates = List[datetime.date]
 Frequencies = List[int]
-
 
 plotly.tools.set_credentials_file(
     username=config.PLOTLY_CONFIG['username'],
@@ -24,19 +22,19 @@ def fromtimestamp(ts: int) -> datetime.date:
     return datetime.datetime.fromtimestamp(ts).date()
 
 
-def count_dates_from_messages(messages: List[Message]) -> Tuple[Dates, Frequencies]:
+def count_dates_from_messages(messages: List[dict]) -> Tuple[Dates, Frequencies]:
     """ Получить список дат и их частот
     :param messages: список сообщений
     """
     dates = []
     x = []
     y = []
-    for value in messages:
-        dates.append(fromtimestamp(value.get('date')))
-    date_counter = Counter(dates)
-    for key, value in date_counter.items():
-        x.append(key)
-        y.append(value)
+    if messages:
+        for value in messages:
+            dates.append(fromtimestamp(value.get('date')))
+        date_counter = Counter(dates)
+        x = list(date_counter.keys())
+        y = list(date_counter.values())
     return x, y
 
 
